5163fb33d833d7de40046b0e1565d97c1de6f7ec
figoaljufrie <figoaldjoeffri@gmail.com>
2025-09-15 23:18:33 +0700

    int; commit

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express, { Application } from "express";
+import cors from "cors";
+
+export class App {
+  private app: Application;
+  private port: number;
+
+  constructor(port: number = 8000) {
+    this.app = express();
+    this.port = port;
+
+    //cors - setup:
+    const corsOptions = {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+      credentials: true,
+    };
+    this.app.use(cors(corsOptions));
+
+    this.app.use(express.json());
+
+    this.initializeRoutes();
+  }
+
+  public initializeRoutes() {
+  }
+
+  public listen() {
+    this.app.listen(this.port, () => {
+      console.log(`Server is running on http://localhost:${this.port}`);
+    });
+  }
+}
+
+const app = new App();
+app.listen();
