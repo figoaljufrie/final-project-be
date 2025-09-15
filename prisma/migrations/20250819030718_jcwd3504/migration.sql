7eef945e845633da04d5a111a7a947519ce3d3d5
imambahy <imambahyp@gmail.com>
2025-09-15 23:29:19 +0700

    add schema prisma

--- prisma/migrations/20250819030718_jcwd3504/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "samples" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "samples_pkey" PRIMARY KEY ("id")
-);
