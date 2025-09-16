0ae58d10e19e8a6ef164dcf16a9646b8f5501627
figoaljufrie <figoaldjoeffri@gmail.com>
2025-09-17 03:54:32 +0700

    feat: user/CRUD

---
+++ src/modules/user/services/user-service.ts
@@ -0,0 +1,63 @@
+import { UserRepository } from "../repository/user-repository";
+import { UserDTO } from "../dto/user-dto";
+import bcrypt from "bcrypt";
+import { ApiError } from "../../../shared/utils/api-error";
+
+export class UserService {
+  userRepository: UserRepository;
+
+  constructor() {
+    this.userRepository = new UserRepository();
+  }
+  //TODO: Move this to auth.service
+  // public async createUser(data: UserDTO) {
+  //   const hashedPassword = await bcrypt.hash(data.password, 10);
+  //   return await this.userRepository.create({
+  //     ...data,
+  //     password: hashedPassword,
+  //     role: "user",
+  //   });
+  // }
+  //TODO: Move this to auth.service
+  // public async createTenant(data: UserDTO) {
+  //   const hashedPassword = await bcrypt.hash(data.password, 10);
+  //   return await this.userRepository.create({
+  //     ...data,
+  //     password: hashedPassword,
+  //     role: "tenant",
+  //   });
+  // }
+
+  public async getAll() {
+    return await this.userRepository.getAll();
+  }
+
+  public async getMe(userId: number) {
+    return await this.userRepository.getMe(userId);
+  }
+
+  public async findById(id: number) {
+    return await this.userRepository.findById(id);
+  }
+
+  public async findByEmail(email: string) {
+    return await this.userRepository.findByEmail(email);
+  }
+
+  public async updateUser(id: number, data: UserDTO) {
+    return await this.userRepository.updateUser(id, data);
+  }
+
+  public async updateAvatar(userId: number, avatarUrl: string) {
+    return await this.userRepository.updateAvatar(userId, avatarUrl);
+  }
+
+  public async updatePassword(userId: number, password: string) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    return await this.userRepository.updatePassword(userId, hashedPassword);
+  }
+
+  public async hardDeleteUser(id: number) {
+    return await this.userRepository.hardDeleteUser(id);
+  }
+}
